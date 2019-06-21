a25f833c14f58cdc94b29646c6be7ff99e789735
DarkWizard-VT <45162334+DarkWizard-VT@users.noreply.github.com>
2019-06-21 11:45:04 +0700

    Update UpdateData.php

--- UpdateData.php
+++ UpdateData.php
@@ -18,38 +18,6 @@ echo "Update database!";
 </ul>
 <?php
 //database info//
-if (empty(getenv("DATABASE_URL"))){
-    echo '<p>The DB does not exist</p>';
-    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
-}  else {
-     echo '<p>The DB exists</p>';
-     echo getenv("dbname");
-   $db = parse_url(getenv("DATABASE_URL"));
-   $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-54-225-72-238.compute-1.amazonaws.com;port=5432;user=ceunivuhcevgju;password=4d325c3e99cf1899edf91c3f49af2bfad5864cbca9124356320bfa86cb111d63;dbname=d475nitcbnrrif",
-        $db["host"],
-        $db["port"],
-        $db["user"],
-        $db["pass"],
-        ltrim($db["path"], "/")
-   ));
-}  
-
-$sql = "SELECT * FROM toystore ORDER BY toyid";
-$stmt = $pdo->prepare($sql);
-//Thiết lập kiểu dữ liệu trả về
-$stmt->setFetchMode(PDO::FETCH_ASSOC);
-$stmt->execute();
-$resultSet = $stmt->fetchAll();
-echo '<p>Toy information:</p>';
-foreach ($resultSet as $row) {
-	echo $row['toyid'];
-        echo "    ";
-        echo $row['toyname'];
-        echo "    ";
-       
-        echo "<br/>";
-}
 
 // database info//
 
